47e3916f79380fbcac257a53bb3e6d751cd1aef7
Plutonium <seksariamanav.2000@gmail.com>
2020-01-07 20:33:28 +0530

    Update observations.js

--- observations.js
+++ observations.js
@@ -1,16 +1,34 @@
 var observations = [
     {
   backgrounds: "https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg",
-  Name:"./pclubs/drama/logo.jpg",
-  Founded:"2006",
+  Name:"All Night Observations",
+  icon: "fas fa-telescope",
   subtext:"Joining drama as a student consultant is a great way to boost your CV with relevant work- and consulting experience, while expanding your professional network. Our consultants get a hands on experience working with clients on a wide range of topics and issues employing strategies to achieve the the firms objective. Moreover, you will expand your social network by meeting like-minded students from different countries, study backgrounds and universities.",
-  },
+},
 {
     backgrounds: "https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg",
-    Name:"./pclubs/ldc/logo.jpg",
-    Founded:"2019",
+    Name:"Star Party",
+    icon: "fas fa-sun",
     subtext:"We’re a bunch of avid readers, funny writers, debaters, loony comedians and more. We value the ability to take a joke higher than the ability to crack one. We find humour in the strangest little things. As far as mixing work and play is concerned, we have thin lines between the two.",
-    }
+},
+{
+    backgrounds: "https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg",
+    Name:"Eclipses",
+    icon: "far fa-circle",
+    subtext:"We’re a bunch of avid readers, funny writers, debaters, loony comedians and more. We value the ability to take a joke higher than the ability to crack one. We find humour in the strangest little things. As far as mixing work and play is concerned, we have thin lines between the two.",
+},
+{
+    backgrounds: "https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg",
+    Name:"Meteor Showers",
+    icon: "fas fa-meteor",
+    subtext:"We’re a bunch of avid readers, funny writers, debaters, loony comedians and more. We value the ability to take a joke higher than the ability to crack one. We find humour in the strangest little things. As far as mixing work and play is concerned, we have thin lines between the two.",
+},
+{
+    backgrounds: "https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg",
+    Name:"Celestial Bodies",
+    icon: "fas fa-globe-europe",
+    subtext:"We’re a bunch of avid readers, funny writers, debaters, loony comedians and more. We value the ability to take a joke higher than the ability to crack one. We find humour in the strangest little things. As far as mixing work and play is concerned, we have thin lines between the two.",
+}
 
 ];
 
@@ -21,7 +39,7 @@ function Obs(observations){
                      <div class="shadow"></div>
                      <div class="label">
                         <div class="icon">
-                           <i class="fas fa-walking"></i>
+                           <i class="${observations.icon}"></i>
                         </div>
                         <div class="info">
                            <div class="main">${observations.Name}</div>
