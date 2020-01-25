294d578dda2945308408dc1292f5423178ce3b78
Dhruv Patidar <50941173+patidardhruv@users.noreply.github.com>
2020-01-26 03:39:35 +0530

    Mobilified NavBar.js

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -1,13 +1,13 @@
 Nav =`
-<div id="mySidebar" class="sidebar" style="height: 100%; width: 0; position: fixed;z-index: 999;top: 0;left: 0;overflow-x: hidden;padding-top: 60px;transition: 0.5s;">
-<div id="background" style="background-color: rgba(51, 51, 51, 0.95);">
-</div>
-<a style="width: 0%;" href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-<a href="index.html">Index</a>
-<a href="events.html">Events</a>
-<a href="observations.html">Observations</a>
-<a href="about.html">About</a>
-<a href="talks.html">Talks</a>
+<div id="mySidebar" class="sidebar">
+  <div id="background" style="">
+  </div>
+
+  <a style="width: 0%;" href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="index.html">Index</a>
+  <a href="events.html">Events</a>
+  <a href="observations.html">Observations</a>
+  <a href="about.html">About</a>
 </div>
 
 <div id="main">
@@ -17,8 +17,20 @@ Nav =`
 document.getElementById("nav-barr").innerHTML = Nav;
 document.getElementById("footer").innerHTML = `<p style="padding:5px;">© 2020 Copyright: Footer</p>`;
 
+var y;
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    y = "100%";
+  } else {
+    y = "250px";
+  }
+}
+
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
+  var x = window.matchMedia("(max-width: 768px)")
+  myFunction(x) // Call listener function at run time
+  document.getElementById("mySidebar").style.width = y;
   document.getElementById("data").style.marginLeft = "-150px";
   document.getElementById("main").style.marginLeft = "250px";
 }
