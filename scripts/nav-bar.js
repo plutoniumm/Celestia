55af2627c52c12e7f2e566bf7bd1e73fd6e0864d
Aviral09 <aviral09@github.com>
2020-01-29 16:22:50 +0530

    Mobilified versions

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -1,25 +1,15 @@
 Nav =`
-<div id="mySidebar" class="sidebar" style="height: 100%; width: 0; position: fixed;z-index: 999;top: 0;left: 0;overflow-x: hidden;padding-top: 60px;transition: 0.5s;">
-<div id="background" style="background-color: rgba(51, 51, 51, 0.95);">
-</div>
-<a style="width: 0%;" href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-<a href="index.html">Index</a>
-<a href="events.html">Events</a>
-<a href="observations.html">Observations</a>
-<a href="talks.html">Talks</a>
-<a href="projects.html">Projects</a>
-<a href="about.html">About</a>
 <div id="mySidebar" class="sidebar">
   <div id="background" style="">
   </div>
-
   <a style="width: 0%;" href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
   <a href="index.html">Index</a>
   <a href="events.html">Events</a>
   <a href="observations.html">Observations</a>
+  <a href="talks.html">Talks</a>
+  <a href="projects.html">Projects</a>
   <a href="about.html">About</a>
 </div>
-
 <div id="main">
   <button style="font-size: 20px;  cursor: pointer;  background-color: rgba(255, 0, 0, 0);  color: white;  padding: 10px 15px;  border: none;" class="openbtn" onclick="openNav()">&#9776; Sidebar</button>
 </div>
